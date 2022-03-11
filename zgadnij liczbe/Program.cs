0a6e391a89111d2314955a3f49be9ef0dc634f44
3110561 <3110561@TE-GDA-D-D26F18>
2022-03-11 15:06:37 +0100

    Dodaj pliki projektów.

---
+++ zgadnij liczbe/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random r = new Random();
+
+int liczba = r.Next(1, 11);
+bool wygrana = false;
+
+Console.WriteLine(liczba);
+Console.WriteLine("Zgadnij liczbę.");
+
+while (!wygrana)
+{
+    int wprowadz = int.Parse(Console.ReadLine());
+    
+    if (wprowadz == liczba)
+    {
+        Console.WriteLine("Wygrałeś!!!");
+        wygrana = true;
+    }
+
+    if (wprowadz < liczba)
+    {
+        Console.WriteLine("Wprowadzona liczba jest mniejsza od wylosowanej.");
+    }
+    else if (wprowadz > liczba)
+    {
+        Console.WriteLine("Wprowadzona liczba jest większa od wylosowanej.");
+    }
+}
